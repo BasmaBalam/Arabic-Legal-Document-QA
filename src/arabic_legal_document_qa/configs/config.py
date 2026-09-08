d678ae91698971d590e8d139cd67024571f04357
BasmaBalam <basmafarajelhadi@gmail.com>
2026-09-08 19:17:26 +0300

    update environment and LLM configuration

--- src/arabic_legal_document_qa/configs/config.py
+++ src/arabic_legal_document_qa/configs/config.py
@@ -6,6 +6,18 @@ class Settings(BaseSettings):
     APP_VERSION: str
     MAX_CIVIL_CODE_ARTICLE_NUMBER: int
     MAX_CHAR_LENGTH_THRESHOLD: int
+    GENERATION_BACKEND: str
+    EMBEDDING_BACKEND: str
+
+    GENERATION_MODEL_ID: str
+    EMBEDDING_MODEL_ID: str
+    EMBEDDING_MODEL_SIZE: int
+
+    DEFAULT_INPUT_MAX_CHARACTERS: int,
+    DEFAULT_GENERATION_MAX_OUTPUT_TOKENS: int,
+    DEFAULT_GENERATION_TEMPERATURE: int,
+    ENABLE_THINKING: bool
+
     
     model_config = SettingsConfigDict(
         env_file="src/arabic_legal_document_qa/.env",
